42aadd9e856766a0d31a2f7bba3166c6432cc5e6
kamranabbasi <rajakamran737@gmail.com>
2021-10-07 12:17:34 +0500

    Kamran's commit

--- src/main/java/com/example/gitprogamcheck/GitprogamcheckApplication.java
+++ src/main/java/com/example/gitprogamcheck/GitprogamcheckApplication.java
@@ -10,14 +10,10 @@ public class GitprogamcheckApplication {
 		SpringApplication.run(GitprogamcheckApplication.class, args);
 
 		System.out.println("deleted all commits ");
-<<<<<<< HEAD
-
 		System.out.println("please don't keep code clean");
 
-=======
 		System.out.println("please keep the code clean");
 		System.out.println("Bhai new class apni bna lo phir");
->>>>>>> a8c4d9b9e2c816d0d7207e7b3b38ec7886d7aa26
 
 	}
 }

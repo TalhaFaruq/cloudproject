7036b88f4bf56dbff212d1906b6ce53175882362
FawadMalik <fawadmalik60@gmail.com>
2021-10-07 12:20:32 +0500

    latest push --fawad2

--- src/main/java/com/example/gitprogamcheck/GitprogamcheckApplication.java
+++ src/main/java/com/example/gitprogamcheck/GitprogamcheckApplication.java
@@ -9,11 +9,7 @@ public class GitprogamcheckApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GitprogamcheckApplication.class, args);
 
-		System.out.println("deleted all commits ");
-		System.out.println("please don't keep code clean");
-
-		System.out.println("please keep the code clean");
-		System.out.println("Bhai new class apni bna lo phir");
+		System.out.println("................. ;-/");
 
 	}
 }

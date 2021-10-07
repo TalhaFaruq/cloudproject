a8c4d9b9e2c816d0d7207e7b3b38ec7886d7aa26
Talha <hafiztalha1997@gmail.com>
2021-10-07 12:15:38 +0500

    talha_commitnew

--- src/main/java/com/example/gitprogamcheck/GitprogamcheckApplication.java
+++ src/main/java/com/example/gitprogamcheck/GitprogamcheckApplication.java
@@ -11,6 +11,7 @@ public class GitprogamcheckApplication {
 
 		System.out.println("deleted all commits ");
 		System.out.println("please keep the code clean");
+		System.out.println("Bhai new class apni bna lo phir");
 
 	}
 }

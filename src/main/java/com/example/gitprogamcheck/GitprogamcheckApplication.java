2e0e42fb74e6a701fc957cfcea28d44af9eb6688
fawadkhan <fawadmalik60@gmail.com>
2021-10-07 12:11:00 +0500

    latest push --fawad2

--- src/main/java/com/example/gitprogamcheck/GitprogamcheckApplication.java
+++ src/main/java/com/example/gitprogamcheck/GitprogamcheckApplication.java
@@ -9,19 +9,8 @@ public class GitprogamcheckApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GitprogamcheckApplication.class, args);
 
-		System.out.println("Hello world...");
-		System.out.println("Kamran' commit");
-		// new changes from fawad
-		System.out.println("hello team by fawad");
-		//new changes from talha
-		System.out.println("Talha Commit");
-
-		GitprogamcheckApplication newteamobj = new GitprogamcheckApplication();
-
-		System.out.println(newteamobj);
-		System.out.println("Kamran' 2nd commit");
-
-		System.out.println("Kamran' 3rd commit on 12:07");
+		System.out.println("deleted all commits ");
+		System.out.println("please keep code clean");
 
 	}
 }

e63f46d03895657c050398fa08042897015a44e0
Haro-stark <haroonrb64@hotmail.com>
2021-10-07 11:26:58 +0500

    inclued hello world sout statemtnt in main function

--- src/main/java/com/example/gitprogamcheck/GitprogamcheckApplication.java
+++ src/main/java/com/example/gitprogamcheck/GitprogamcheckApplication.java
@@ -8,6 +8,7 @@ public class GitprogamcheckApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GitprogamcheckApplication.class, args);
-	}
+		System.out.println("Hello world...");
 
+	}
 }

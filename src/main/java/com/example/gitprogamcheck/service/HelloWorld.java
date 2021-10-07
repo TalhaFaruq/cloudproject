4132f8d8a5b449a16037c01e6bb7c7c1daf497b5
RaisAhmad <raisahmadd7@gmail.com>
2021-10-07 12:00:43 +0500

    Initial changes

--- src/main/java/com/example/gitprogamcheck/service/HelloWorld.java
+++ src/main/java/com/example/gitprogamcheck/service/HelloWorld.java
@@ -7,4 +7,8 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello team");
     }
+
+    public String getName() {
+        return name;
+    }
 }
